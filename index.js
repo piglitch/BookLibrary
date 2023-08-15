fff3017485eb209670522ec8bc3507556048a1bd
Avi <avibanerjee2000@gmail.com>
2023-08-15 20:24:07 +0530

    Book-> add, delete, read/unread, booklink, styling

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let myLibrary = []
 
 // Constructor
-function Book(bookName){
+function Book(bookName, btn, bookLink){
     this.bookName = bookName
-    //this.bookLink = bookLink
-
+    this.bookLink = bookLink
+    this.btn = btn
 }
 
 // Function that adds books
@@ -14,19 +14,15 @@ const addBookToLibrary = () => {
     
     // Takes input 
     const bname = document.getElementById("name").value;
-    //const blink = document.getElementById("bookLink").value;
+    const blink = document.getElementById("bookLink").value;
+    var readUnread = document.createElement("button");
+    readUnread.type = 'button';
 
     // Makes a new object
-    const book = new Book(`${bname}`)
+    const book = new Book(`${bname}`, readUnread, `${blink}`)
     
     // Pushes into the library
-    myLibrary.push(book.bookName)
-    
-
-    console.log(myLibrary);
-
-    
-
+    myLibrary.push(book)
     })};
 
 addBookToLibrary();
@@ -38,16 +34,57 @@ const displayBooks = () => {
   
     for (let i = 0; i < myLibrary.length; i++) {
         
-      const bookName = myLibrary[i];
+      const book = myLibrary[i];
 
-      if (!displayedBooks.has(bookName)) {
-      const bookElement = document.createElement("li");
-      bookElement.textContent = bookName;
+      if (!displayedBooks.has(book.bookName)) {
+
+      // For books  
+      const bookElement = document.createElement("ul");
+      bookElement.style.display = 'flex'
+      bookElement.textContent = book.bookName;
+      bookElement.setAttribute('book-name', book.bookName);
+      bookElement.classList.add('glow-border');
+      listOfBooksElement.style.padding = '5px'
       listOfBooksElement.appendChild(bookElement);
 
+      // Booklink
+      bookElement.addEventListener("click", ()=>{
+        window.open(`${book.bookLink}`);    
+      })
+
+      // Read and Unread button  
+      const readUnread = document.createElement("button");
+      readUnread.type = 'button';
+      readUnread.classList.add('readUnread');
+      readUnread.textContent = book.read ? "👁️" : "🙈";
+      bookElement.appendChild(readUnread);      
+      
+      // To toggle between 'Read' and 'Unread'
+      readUnread.addEventListener('click', (event) => {
+        event.stopPropagation();
+        book.read = !book.read;
+        readUnread.textContent = book.read ? "👁️" : "🙈";
+      })
+
+      // Delete button
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "❎"
+      bookElement.appendChild(deleteButton);
+      deleteButton.setAttribute('book-remove', i)
+
+      // Delete evenListener
+      deleteButton.addEventListener("click", (event)=>{
+        event.stopPropagation();
+        const bookIndex = parseInt(deleteButton.getAttribute('book-remove'));
+        if(!isNaN(bookIndex) && bookIndex >= 0 && bookIndex < myLibrary.length){
+          myLibrary.splice(bookIndex, 1);
+          displayBooks();
+        }
+      })
+
       // Add the book to the displayed set
-      displayedBooks.add(bookName);
-    }
+      displayedBooks.add(book.bookName);
+      }      
     }
   };
   
